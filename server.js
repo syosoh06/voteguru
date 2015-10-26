1c3d61ff97bba6d50c3c2e402b5648367d12215a
syosoh06 <sohamc_com@icloud.com>
2015-10-26 07:09:33 -0400

    tried playing around with http to get the CRUD working

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use(bodyParser.json());
 // configure our app to handle CORS requests
 app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
     next();
 });
